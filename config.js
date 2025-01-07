f111351494cb8864c46d49cb030b0acd937bc324
ᗧ···ᗣ···ᗣ <54386876+ColinRitman@users.noreply.github.com>
2025-01-07 03:28:18 -0500

    Update config.js
    localhost

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-var api = 'http://fango.money:18180';
-var poolListUrl = 'https://fangotango.hopto.org/api/pools/data';
+var api = 'http://127.0.0.1:18180';
+var poolListUrl = '';
 var donationAddress = "";
 var blockTargetInterval = 480;
 var coinUnits = 10000000;
